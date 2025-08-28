925a73fab8a2b19e0aeed99d093aa52e03705792
Jian Lee <ramosjianlee@gmail.com>
2025-08-28 22:48:01 +0800

    Refactor server.js to include PostgreSQL pool configuration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+//Basic Server file for PERN Stack
 const express = require("express");
 const cors = require("cors");
+//PostGreSQL pool for database
+const { Pool } = require("pg");
 
 const app = express();
 
@@ -9,3 +12,14 @@ app.use(express.json());
 app.listen(500, () => {
   console.log("Server running on http://localhost:500");
 });
+
+//Connect to PostGreSQL database
+const pool = new Pool({
+  user: "jianlee",
+  host: "localhost",
+  database: "pern_todo",
+  password: "",
+  port: 5432,
+});
+
+modules.export = pool;
