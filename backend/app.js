4fc1f32b5181195fdfec12ca943ac9faec224c2c
Jian Lee <ramosjianlee@gmail.com>
2025-09-05 00:00:44 +0800

    initialized cors on backend for frontend to connect

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 
 //Middleware to parse JSON
 app.use(express.json());
+
+//Enable CORS
+app.use(cors());
 
 //Middleware for URL-encoded data
 app.use(express.urlencoded({ extended: true }));
