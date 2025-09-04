37b67c83c9b7a6339960559aa2ef058da35c03ce
Jian Lee <ramosjianlee@gmail.com>
2025-09-04 19:17:46 +0800

    Add password field to database connection pool configuration

--- backend/db/index.js
+++ backend/db/index.js
@@ -6,6 +6,7 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
