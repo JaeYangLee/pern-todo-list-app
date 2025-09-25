8f0168d96e57d1d29fbb1f61e998e8d9a78e1a87
Jian Lee <ramosjianlee@gmail.com>
2025-09-25 15:46:30 +0800

    fix: Styling Tweaks

--- frontend/src/components/ToDoFooter.jsx
+++ frontend/src/components/ToDoFooter.jsx
@@ -5,12 +5,12 @@ function ToDoFooter() {
     <>
       <footer className="flex items-center justify-center w-full px-4 py-2 font-mono bg-black md:justify-between">
         <section>
-          <p className="text-[8px] text-white lg:text-lg xl:text-2xl">
+          <p className="text-[8px] text-white lg:text-xs xl:text-sm">
             © 2025 Jian Lee Ramos. All rights reserved.
           </p>
         </section>
-        <section className="hidden">
-          <p className="text-xs text-white">Built with PERN</p>
+        <section className="hidden lg:flex">
+          <p className="text-xs text-white xl:text-sm">Built with PERN</p>
         </section>
       </footer>
     </>
--- frontend/src/components/ToDoForm.jsx
+++ frontend/src/components/ToDoForm.jsx
@@ -18,9 +18,11 @@ function ToDoForm({ onAdd }) {
 
   return (
     <>
-      <div className="flex flex-col items-start justify-start font-mono bg-[#ffedd6] border-b-1 border-black py-2">
+      <div className="flex flex-col items-start justify-start font-mono bg-[#ffedd6] border-b-1 border-black py-2 lg:p-3 xl:p-4">
         <header className="flex flex-col items-start justify-start p-2">
-          <h1 className="text-2xl font-bold lg:text-4xl">TO DO LIST</h1>
+          <h1 className="text-2xl font-bold lg:text-5xl xl:text-5xl">
+            TO DO LIST
+          </h1>
         </header>
         <form onSubmit={handleSubmit} className="flex flex-col gap-2 p-1">
           <div>
@@ -29,19 +31,19 @@ function ToDoForm({ onAdd }) {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               placeholder="Enter your task..."
-              className="w-[60vw] text-sm px-2 py-1 bg-[#ffedd6] border-black border-1"
+              className="w-[60vw] text-sm px-2 py-1 bg-[#ffedd6] border-black border-1 xl:text-1xl xl:p-3"
             />
           </div>
           <div className="flex flex-row gap-4 px-2 py-2 text-sm">
             <button
               type="submit"
-              className="px-2 text-sm border-black border-1 shadow-[2px_2px_0px_0px_rgba(0,0,0,0.75)] hover:bg-[#ccbeab] lg:text-lg"
+              className="px-2 text-sm border-black border-1 shadow-[2px_2px_0px_0px_rgba(0,0,0,0.75)] hover:bg-[#ccbeab] lg:text-lg xl:text-1xl"
             >
               Add
             </button>
             <button
               onClick={handleResetInput}
-              className="px-2 text-sm border-black border-1 shadow-[2px_2px_0px_0px_rgba(0,0,0,0.75)] hover:bg-[#ccbeab] lg:text-lg"
+              className="px-2 text-sm border-black border-1 shadow-[2px_2px_0px_0px_rgba(0,0,0,0.75)] hover:bg-[#ccbeab] lg:text-lg xl:text-1xl"
             >
               Clear
             </button>
