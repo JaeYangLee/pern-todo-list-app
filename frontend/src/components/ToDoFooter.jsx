accf42b1852fa150359bfcaf70aab3e452f952d0
Jian Lee <ramosjianlee@gmail.com>
2025-09-26 04:34:53 +0800

    fix: add select-none class to ToDoFooter for better text selection handling

--- frontend/src/components/ToDoFooter.jsx
+++ frontend/src/components/ToDoFooter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ToDoFooter() {
   return (
     <>
-      <footer className="flex items-center justify-center w-full px-4 py-2 font-mono bg-black md:justify-between">
+      <footer className="flex items-center justify-center w-full px-4 py-2 font-mono bg-black select-none md:justify-between">
         <section>
           <p className="text-[8px] text-white lg:text-xs xl:text-sm">
             © 2025 Jian Lee Ramos. All rights reserved.
