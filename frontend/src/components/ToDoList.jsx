63fcda3b65fe8eb3e0b9468598c2820eaf0d5735
Jian Lee <ramosjianlee@gmail.com>
2025-09-26 04:23:33 +0800

    fix: improve ToDoList component to handle empty todos state with a message

--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -4,16 +4,24 @@ import ToDoItem from "./ToDoItem";
 function ToDoList({ todos = [], onUpdate, onDelete }) {
   return (
     <>
-      <ul className="list-disc flex flex-col items-center justify-between py-2 bg-[#ffedd6] border-transparent ">
-        {todos.map((todos) => (
+      <ul className="list-disc flex flex-col items-center justify-between py-2 bg-[#ffedd6] border-transparent "></ul>
+      {todos.length === 0 ? (
+        <div className="flex items-center justify-center">
+          <p className="flex flex-col items-center justify-center opacity-50">
+            No tasks yet. <span>Add one to get started!</span>
+          </p>
+        </div>
+      ) : (
+        todos.map((todos) => (
           <ToDoItem
             key={todos.todo_id}
             todos={todos}
             onUpdate={onUpdate}
             onDelete={onDelete}
+            className="z-50"
           />
-        ))}
-      </ul>
+        ))
+      )}
     </>
   );
 }
