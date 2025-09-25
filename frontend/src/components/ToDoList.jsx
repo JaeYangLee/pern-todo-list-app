2112f98b1ad1349d9bc2781c6ca4de49670523f7
Jian Lee <ramosjianlee@gmail.com>
2025-09-26 04:35:02 +0800

    fix: update empty todos message in ToDoList for improved user experience

--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -7,8 +7,9 @@ function ToDoList({ todos = [], onUpdate, onDelete }) {
       <ul className="list-disc flex flex-col items-center justify-between py-2 bg-[#ffedd6] border-transparent "></ul>
       {todos.length === 0 ? (
         <div className="flex items-center justify-center">
-          <p className="flex flex-col items-center justify-center opacity-50">
-            No tasks yet. <span>Add one to get started!</span>
+          <p className="flex flex-col items-center justify-center select-none opacity-20 lg:text-2xl xl:text-3xl">
+            You’ve got nothing here…
+            <span>except free time</span>
           </p>
         </div>
       ) : (
